071fb7e2506ae3eed89725bc3381d1a0d3c4fb2f
seventhcepheus <audrey270700@gmail.com>
2025-05-15 18:33:57 +0300

    Подготовлено приложение для GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "./",
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  }
+})
+
+
+
